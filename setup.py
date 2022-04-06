88673883533c9828d1da453c5814673dcbf9e804
CedricHermansBIT <cedric.hermans2@howest.be>
2022-04-06 11:54:00 +0200

    Change setup version

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
 import setuptools
 from setuptools import setup
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='gymgrid',
-	  version='1.0.6',
-	  author="Ruoqi Zhang",
-	  author_email="ruoqi.zhang.666@gmail.com",
-	  long_description=long_description,
-	  long_description_content_type="text/markdown",
-	  url="https://github.com/wsgdrfz/gym-gridworld",
-	  packages=setuptools.find_packages(),
-	  python_requires='>=3.5',
-	  classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+      version='1.2.0',
+      author="Ruoqi Zhang",
+      author_email="ruoqi.zhang.666@gmail.com",
+      maintainer="Cedric Hermans",
+      maintainer_email="cedric_hermans@hotmail.com",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/CedricHermansBIT/gymgrid",
+      packages=setuptools.find_packages(),
+      python_requires='>=3.5',
+      classifiers=[
+              "Programming Language :: Python :: 3",
+              "License :: OSI Approved :: MIT License",
+              "Operating System :: OS Independent",
       ],
-	  install_requires=['gym', 'numpy']
-)
+      install_requires=['gym', 'numpy']
+      )
